8ea16d19698682699624108cfc10f1867afb817f
danieltdang <dtd21b@fsu.edu>
2024-01-25 10:26:38 -0500

    Added card overlay and formatting

--- client/components/Content.js
+++ client/components/Content.js
@@ -9,14 +9,19 @@ const Content = () => {
   };
 
   const data = [
-    { id: '1', title: 'Item 1' },
-    { id: '2', title: 'Item 2' },
+    { id: '1', title: 'Item 1', club: 'Club 1', date: 'WED, JAN 25 AT 10 PM', interested: '69K interested'},
+    { id: '2', title: 'Item 2', club: 'Club 2', date: 'WED, JAN 26 - 28', interested: '4.2K interested'},
     // ... more items
   ];
 
   const renderItem = ({ item }) => (
     <View style={cardImageStyle}>
-      <Text>{item.title}</Text>
+      <View style={overlayStyle}>
+        <Text style={{ fontSize: 10, fontWeight: 600 }}>{item.date}</Text>
+        <Text style={{ fontSize: 13, fontWeight: 600 }}>{item.title}</Text>
+        <Text style={{ fontSize: 12, fontColor: '#455154' }}>{item.club}</Text>
+        <Text style={{ fontSize: 12, fontColor: '#455154' }}>{item.interested}</Text>
+      </View>
     </View>
   );
 
@@ -24,25 +29,38 @@ const Content = () => {
     flex: 1,
     justifyContent: "space-around",
     marginHorizontal: 25,
+    marginTop: 40, 
   };
   const cardImageStyle = {
     flex: 1,
     marginHorizontal: 10,
     paddingVertical: 100,
     paddingHorizontal: 70,
-    innerWidth: 145,
-    innerHeight: 200,
-    outerHeight: 200,
+    width: 150,
+    height: 250,
     borderWidth: 1,
-    borderColor: '#ddd',
+    borderColor: '#D9D9D9',
     borderRadius: 10,
-    backgroundColor: '#fff',
+    backgroundColor: '#D9D9D9',
     shadowColor: 'rgba(0,0,0, 0.25)',
     shadowOpacity: 0.5,
     shadowOffset: { width: 0, height: 4 },
     shadowRadius: 4,
     elevation: 4
   };
+  const overlayStyle = {
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    right: 0,
+    height: 80,
+    paddingHorizontal: 10,
+    backgroundColor: 'white',
+    justifyContent: 'center',
+    alignItems: 'left',
+    borderBottomLeftRadius: 10, // Match the border radius of the parent View
+    borderBottomRightRadius: 10,
+};
 
     return (
         <View style={containerStyle}>
