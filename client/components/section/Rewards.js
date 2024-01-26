d9205dcdf0404039930083e7710636960a491f23
mattechenique <mme21a@fsu.edu>
2024-01-26 16:15:36 -0500

    adding data to the rewards page

--- client/components/section/Rewards.js
+++ client/components/section/Rewards.js
@@ -8,6 +8,13 @@ const Rewards = () => {
     backgroundColor: 'white',
   };
 
+  const data = [
+    { id: 0, title: 'Rewards 1', icon: <Image source = {require('../../assets/fsu_coins.png')} style={{width: 20, height: 20}}/>, rewardAmount: 200 },
+    { id: 1, title: 'Rewards 2', icon: <Image source = {require('../../assets/fsu_coins.png')} style={{width: 20, height: 20}}/>, rewardAmount: 300 },
+    { id: 2, title: 'Rewards 3', icon: <Image source = {require('../../assets/fsu_coins.png')} style={{width: 20, height: 20}}/>, rewardAmount: 500 },
+    { id: 3, title: 'Rewards 4', icon: <Image source = {require('../../assets/fsu_coins.png')} style={{width: 20, height: 20}}/>, rewardAmount: 1000 },
+  ];
+
   return (
     <View style={containerStyle}>
       <Text> Rewards </Text>
