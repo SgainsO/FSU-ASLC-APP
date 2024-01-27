7ab0180604022e87965bf20b28be0d76ba089aed
Jas Chawla <jptjasc@gmail.com>
2024-01-27 16:53:37 -0500

    Changed Cors Settings (Get Requests Now Work)  #2
    Post requests still do not work.

--- server/index.js
+++ server/index.js
@@ -1,15 +1,25 @@
 require('dotenv').config();
 
+
 const express = require('express');
 const mongoose = require('mongoose');
 
-
+const rea = require('./models/Read');
 const mongoString = process.env.DATABASE_URL;
 
 // Import your routes
 const routes = require('./routes/routes');
 
 const app = express();
+
+app.use(function(req, res, next) {             // CORS middleware
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+
+
 app.use(express.json()); //Utilize express json middleware to turn req.body to json
 app.use('/api', routes); // localhost:8080/api
 
