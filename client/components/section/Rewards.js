ee256c5455d271b349202362b80b715e1d402a9e
Matthew Echenique <mme21a@fsu.edu>
2024-01-28 21:57:34 -0500

    fixed formatting and return type issues

--- client/components/section/Rewards.js
+++ client/components/section/Rewards.js
@@ -1,6 +1,6 @@
-import { View, FlatList, Text } from 'react-native';
+import { View, FlatList, Text, Image } from 'react-native';
 
-import Card from '../RewardsCard';
+import RewardsCard from '../RewardsCard';
 
 const Rewards = () => {
   const containerStyle = {
@@ -8,19 +8,32 @@ const Rewards = () => {
     backgroundColor: 'white',
   };
 
+  const rowStyle = {
+    flex: 1,
+    justifyContent: "space-around",
+    marginHorizontal: 25,
+    marginVertical: 10,
+  };
+
   const data = [
-    { id: 0, title: 'Rewards 1', icon: <Image source = {require('../../assets/fsu_coins.png')} style={{width: 20, height: 20}}/>, rewardAmount: 200 },
-    { id: 1, title: 'Rewards 2', icon: <Image source = {require('../../assets/fsu_coins.png')} style={{width: 20, height: 20}}/>, rewardAmount: 300 },
-    { id: 2, title: 'Rewards 3', icon: <Image source = {require('../../assets/fsu_coins.png')} style={{width: 20, height: 20}}/>, rewardAmount: 500 },
-    { id: 3, title: 'Rewards 4', icon: <Image source = {require('../../assets/fsu_coins.png')} style={{width: 20, height: 20}}/>, rewardAmount: 1000 },
+    { id: 0, title: 'Reward 1', icon: <Image source = {require('../../assets/fsu_coins.png')} style={{width: 20, height: 20}}/>, rewardAmount: 200 },
+    { id: 1, title: 'Reward 2', icon: <Image source = {require('../../assets/fsu_coins.png')} style={{width: 20, height: 20}}/>, rewardAmount: 300 },
+    { id: 2, title: 'Reward 3', icon: <Image source = {require('../../assets/fsu_coins.png')} style={{width: 20, height: 20}}/>, rewardAmount: 500 },
+    { id: 3, title: 'Reward 4', icon: <Image source = {require('../../assets/fsu_coins.png')} style={{width: 20, height: 20}}/>, rewardAmount: 1000 },
   ];
 
   return (
     <View style={containerStyle}>
+      <FlatList
+      data ={data}
+      renderItem ={({item}) => <RewardsCard {...item}/>}
+      keyExtractor={item => item.id}
+      numColumns={2}
+      columnWrapperStyle={rowStyle}
+      />
       <Text> Rewards </Text>
     </View>
   );
 };
 
 export default Rewards;
-
