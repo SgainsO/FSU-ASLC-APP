16368120687628c1c583dfbb1af9d3943757e266
Matthew Echenique <mme21a@fsu.edu>
2024-01-30 18:49:08 -0500

    adding actual header (not in flexview)

--- client/components/section/Rewards.js
+++ client/components/section/Rewards.js
@@ -23,24 +23,56 @@ const Rewards = () => {
     { id: 3, title: 'Reward 4', icon: <Image source = {require('../../assets/fsu_coins.png')} style={{width: 20, height: 20}}/>, rewardAmount: 1000 },
   ];
 
-  const header = [
-    {id: 'header1', component: <Text style={{fontSize: 16, fontWeight: '600', paddingLeft: 32}}>MY REWARDS</Text>},
+  /*
+  const header [
+  {id: 'header1', component: <Text style={{fontSize: 16, fontWeight: '600', paddingLeft: 32}}>MY REWARDS</Text>},
     { id: 'header3', component: <Text style={{fontSize: 16, fontWeight: '600', paddingLeft: 32}}>REWARDS STORE</Text>}
   ];
+  */
+  
 
   const styles = StyleSheet.create({
     centeredBox: {
       flex:1, 
       justifyContent: 'center',
       alignItems: 'center',
+    },
+    header: {
+      flexDirection: 'row',
+      alignItems: 'left'
+    },
+    icon: {
+      width: 40,
+      height: 40, 
+      marginLeft: 15,
+      marginTop: 5,
+    },
+    RewardsAmount: {
+      fontSize: 40,
+      fontWeight: '600',
+      paddingLeft: 10
+    },
+    RewardsHistory: {
+      fontSize: 22,
+      fontWeight: '600',
+      paddingLeft: 50,
+      paddingTop: 13
     }
   });
 
   return (
     <View style={containerStyle}>
+      <View style={styles.header}>
+      <Image source={require('../../assets/fsu_coins.png')} style={styles.icon} />
+      <Text style={styles.RewardsAmount}>180</Text>
+      <Text style={styles.RewardsHistory}>Rewards History {'>'}</Text>
+      </View>
+      <View style={{ borderBottomColor: 'rgba(0, 0, 0, 0.1)', borderBottomWidth: 1, marginVertical: 10 }} />
+      <Text style={{fontSize: 16, fontWeight: '600', paddingLeft: 32, paddingBottom: 35, paddingTop: 5}}>MY REWARDS</Text>
       <View style={styles.centeredBox}>
         <RoundedBox />
       </View>
+      <Text style={{fontSize: 16, fontWeight: '600', paddingLeft: 32, paddingTop: 43}}>REWARDS STORE</Text>
       <FlatList
       data ={data}
       renderItem ={({item}) => <RewardsCard {...item} />}
