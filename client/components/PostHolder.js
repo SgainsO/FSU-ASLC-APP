75f8a2719263fdac8d6fcbb9fc980a5a2a6d8440
Jas Chawla <jptjasc@gmail.com>
2024-02-07 23:25:27 -0500

    #11 Changed Icon Design, Added Image Switching

--- client/components/PostHolder.js
+++ client/components/PostHolder.js
@@ -1,11 +1,17 @@
-import { Dimensions, StyleSheet, Text, View, Button, Image } from 'react-native';
-import { ImageBackground } from 'react-native-web';
+import { useState } from 'react';
+import { Dimensions, StyleSheet, Text, View, Button, Image, Touchable } from 'react-native';
+import { ImageBackground, TouchableOpacity } from 'react-native-web';
 const {width, height} = Dimensions.get('window');
 
 //Back Color is used to keep the holder the same color as callers background
-PostWidth = width * 1/3;
+PostWidth = width * 1/2;
+iconHolderWidth = PostWidth * 1/5;
+iconWidth = iconHolderWidth * 1/2;
 const Icons = [require("../assets/Like.png"), require("../assets/message.png"),
 require("../assets/Bookmark.png"), require("../assets/Share.png")]
+
+PostHeight = height * 1;
+
 
 const PostHolder = (Club, Date, BackColor) => {
     const Styles = StyleSheet.create({
@@ -23,7 +29,7 @@ const PostHolder = (Club, Date, BackColor) => {
             backgroundColor: 'ffffff',
             alignItems: 'center',
             justifyContent: 'center',
-            width: width * 1/2,
+            width: PostWidth,
             borderRadius: 40,
             aspectRatio: 1,
 
@@ -39,25 +45,51 @@ const PostHolder = (Club, Date, BackColor) => {
         },
         ImageStyle:
         {
-            width: width * 1/2, 
+            width: PostWidth, 
             aspectRatio: 1,
             background: 'transparent',
             alignSelf: 'left'
         },
-        VectorStyle:
+        VectorStyle:             // Holds the icon
         {
-            flex: 1,
+            justifyContent: 'space-between',
+            alignItems: 'center',
             position: 'absolute',
-            top: '40%',
-            left: '95%',
+            width: '50%',
+            height: '20%',
+            flexDirection: 'row',
+            top: '80%',
+            left: '25%',
         },
         IconStyle:
         {
-            marginBottom: 5,
-            width: '50%',
+            flex: 1,
+            marginBottom: 1,
+            width: iconWidth,
             height: undefined,
             aspectRatio: 1,
             resizeMode: 'contain'
+        },
+        GoLeft:
+        {
+            position: 'absolute',
+            top: 0,
+            left: 0,
+            flex: 1,
+            height: height,
+            width: PostWidth * 1/4,
+
+        },
+        GoRight:
+        {
+            position: 'absolute',
+            top: 0,
+            right: 0,
+     //       backgroundColor: 'black',
+            flex: 1,
+            height: height,
+            width: PostWidth * 1/4,
+            transform: [{scaleX: -1}]
         }
 /*        ButtonEdge:
        {
@@ -68,9 +100,11 @@ const PostHolder = (Club, Date, BackColor) => {
 */
     });
 
+    
+
     const IconView = ({ icons, spacing = 10 }) => {
         return (
-          <View style={{ flexDirection: 'column', justifyContent: 'space-between' }}>
+          <View style={Styles.VectorStyle}>
             {icons.map((icon, index) => (
               <Image key={index} source={icon} style={Styles.IconStyle}  />
             ))}
@@ -78,18 +112,39 @@ const PostHolder = (Club, Date, BackColor) => {
         );
       };
 
-      
-return (
-    
+      const TestPhotos = [require("../assets/Daniel.jpg"), require("../assets/Ryan.jpg")]
+      const [Image_Source, ChaImageSource] = useState(TestPhotos[0])
+      const [ImageIndex, ChangeImageIndex] = useState(0)
 
+      const GoForeward = () =>
+      {
+        ImageIndex < (TestPhotos.length - 1) ? ChangeImageIndex(ImageIndex + 1) : ChangeImageIndex(0); 
+        console.log("Going to " + ImageIndex)
+        ChaImageSource(TestPhotos[ImageIndex])
+      } 
+
+      const GoBackward = () =>
+      {
+        ImageIndex > 0 ? ChangeImageIndex(ImageIndex - 1) : ChangeImageIndex(TestPhotos.length - 1); 
+        console.log("Going to " + ImageIndex)
+        ChaImageSource(TestPhotos[ImageIndex])
+      }
+
+return (
     <View style = {Styles.PostHolderStyle}>
         <Image
         style = {Styles.ImageStyle}
-        source = {require('../assets/Daniel.jpg')}
+        source = {Image_Source}
         resizeMode= {'contain'} />
-        <View style = {Styles.VectorStyle}>
-            <IconView icons={Icons} spacing={10} />
-        </View>
+        <IconView icons={Icons} spacing={10} />         
+        <TouchableOpacity onPress = {GoBackward} style = {Styles.GoLeft}>
+        </TouchableOpacity> 
+        
+        <TouchableOpacity onPress={GoForeward} style = {Styles.GoRight}>
+        
+        </TouchableOpacity>
+
+
     </View>
   
  
@@ -102,24 +157,3 @@ return (
 };
 
 export default PostHolder;
-
-
-/*
-    <Image style ={{paddingBottom : 100}}
-            source={ require('../assets/Bookmark.png')} />
-         </View>
-         <View style = {Styles.RightButtonStyle}>
-            <Image
-             style = {Styles.VectorStyle}
-             source={require('../assets/Share.png')} />
-          </View>
-          <View style = {Styles.RightButtonStyle}>
-            <Image
-            style = {Styles.VectorStyle}
-            source={require('../assets/message.png')} />
-            </View>
-         <View style = {Styles.RightButtonStyle}>
-            <Image 
-            style = {Styles.VectorStyle}
-            source={require('../assets/Like.png')} />
-*/
