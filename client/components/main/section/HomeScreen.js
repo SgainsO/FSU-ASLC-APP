245d11c43d459ca69b32dee803bc65b1d2ee5c66
mattechenique <122398635+mattechenique@users.noreply.github.com>
2024-02-25 21:11:47 -0500

    finishing up the final touches to homescreen
    scroll still needs to be implemented

--- client/components/main/section/HomeScreen.js
+++ client/components/main/section/HomeScreen.js
@@ -1,5 +1,6 @@
 import { View, FlatList, Text, } from 'react-native';
 import React from 'react';
+import Card from '../HomeEventCard';
 
 const HomeScreen = () => {
   const getTodayDate = () => {
@@ -18,14 +19,14 @@ const HomeScreen = () => {
     fontSize: 30,
     fontWeight: '600',
     textAlign: 'center',
+    paddingTop: 20,
+    paddingBottom: 20,
   };
 
-  const rowStyle = {
-    flex: 1,
-    justifyContent: "space-around",
-    marginHorizontal: 25,
-    marginVertical: 10,
-  };
+  const data = [
+    {id: 0, title: 'American Psycho', backgroundImage: require('../../assets/american_psycho.png'), interested: '69 interested', details: 'doors open at 11:00pm\nmovie starts at 12:00am',},
+    {id: 1, title: 'Undertale Game Session', backgroundImage: require('../../assets/sans_undertale.png'), interested: '420 interested', details: 'starts at 2pm in theater',},
+  ];
 
   return (
     <View style={containerStyle}>
@@ -33,6 +34,17 @@ const HomeScreen = () => {
         Today is...{'\n'}
         {getTodayDate()}
       </Text>
+      <FlatList
+        data={data}
+        renderItem={({ item }) => (
+          <View style={{ flex: 1 }}>
+            <Card {...item} />
+          </View>
+        )}
+        keyExtractor={item => item.id.toString()}
+        vertical
+        contentContainerStyle={{ paddingHorizontal: 16}}
+        />
       <Text> Home </Text>
     </View>
   );
