3f6c734a2a1536d3e22d563ab582a7621131fcc7
Ryan Nageer <rfn22@fsu.edu>
2024-03-04 12:34:25 -0500

    Added in changes based on new file structure from main

--- client/components/main/Header.js
+++ client/components/main/Header.js
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
-import { Text, Image, View, TouchableWithoutFeedback } from 'react-native';
+import { Text, Image, View, TouchableWithoutFeedback, StyleSheet, KeyboardAvoidingView, Platform, Button, TouchableOpacity } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
-
+import Modal from 'react-native-modal';
+import Animatable from 'react-native-animatable';
 import Icon from './Icon'
 
 const Header = () => {
@@ -31,17 +32,85 @@ const Header = () => {
         navigation.navigate(navigateTo);
     };
 
+    const [isModalVisible, setModalVisible] = useState(false);
+  
+    const toggleModal = () => {
+      setModalVisible(!isModalVisible);
+    };
+
+   
+
+    function freq()
+    {
+        setModalVisible(false);
+        handleIconPress('FAQs', 'FAQs')
+    }
+    function sett()
+    {
+        setModalVisible(false);
+        handleIconPress('Sett', 'Sett')
+    }
+
     return (
         <View style={barStyle}>
-            <Image source={require('../../assets/fsu_logo.png')} style={{ width: 50, height: 50, margin: 4 }} />
+            <Image source={require('../assets/fsu_logo.png')} style={{ width: 50, height: 50, margin: 4 }} />
             <Text style={{ color: 'white', fontSize: 30, fontWeight: 600, marginBottom: 8 }}>ASLC Connect</Text>
             <Icon
                 iconStyle={iconStyle}
-                iconSource={activeIcon === 'Home' ? require('../../assets/burger_menu.png') : require('../../assets/burger_menu.png')}
-                onPress={() => handleIconPress('FAQs', 'FAQs')}
+                iconSource={activeIcon === 'Home' ? require('../assets/burger_menu.png') : require('../assets/burger_menu.png')}
+                onPress={() => setModalVisible(true)}
             />
+
+            <Modal
+                animationIn="slideInLeft"
+                animationOut="slideOutRight"
+                visible = {isModalVisible}
+                onBackdropPress={toggleModal}
+                style={styles.modal}
+                >
+                <KeyboardAvoidingView behavior={Platform.OS === "ios" ? "padding" : "height"} style={{flex: 0, backgroundColor: 'rgba(0, 0, 0, 0.5)',}}>
+                    <View style={styles.container}>
+
+                
+                    <TouchableOpacity onPress={() => sett()}>
+            <Text style={{color:'white', fontSize: 20, fontWeight: 'bold', }}>{'\n'}Settings</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity onPress={() => freq()}>
+            <Text style={{color:'white', fontSize: 16, fontWeight: 'bold', }}>{'\n'}Frequently Asked Questions</Text>
+          </TouchableOpacity>
+                    
+                    </View>
+                    </KeyboardAvoidingView>
+            </Modal>
         </View>
     );
 };
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 3,
+        fontWeight: 'bold',
+        color: 'white',
+        backgroundColor: '#782F40',
+        justifyContent: 'flex-start',
+        alignItems: 'center',
+        overflow: 'visible',
+    },
+    modal: {
+        marginLeft: 10,
+        marginRight: 0,
+        marginBottom: 104,
+        marginTop: 104,
+        flex: 1,
+        alignItems: 'flex-end',
+        justifyContent: 'space-evenly',
+        backgroundColor: 'rgba(0, 0, 0, 0)', // transparent background
+        
+      },
+      title: {
+      fontWeight: 'bold',
+      }
+    });
 
 export default Header;
---
+++ client/components/main/dropdown/Sett.js
@@ -0,0 +1,102 @@
+import React, { useState } from 'react';
+import { View, Text, Image, StyleSheet, ScrollView,  TouchableOpacity } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+
+
+    
+
+    
+
+const WordWithLine = ({ word, onPress }) => {
+    return (
+        <TouchableOpacity onPress={onPress} style={styles.container}>
+      <View style={styles.container}>
+        <Text style={styles.word}>{word}</Text>
+        <View style={styles.line} />
+      </View>
+      </TouchableOpacity>
+    );
+  };
+  const WordWithLineRed = ({ word, onPress }) => {
+    return (
+        <TouchableOpacity onPress={onPress} style={styles.container}>
+      <View style={styles.container}>
+        <Text style={styles.word2}>{word}</Text>
+        <View style={styles.line} />
+      </View>
+      </TouchableOpacity>
+    );
+  };
+
+const Sett = () => {
+    const navigation = useNavigation();
+    
+    const [activeIcon, setActiveIcon] = useState("Home");
+
+    const handleIconPress = (iconName, navigateTo) => {
+        setActiveIcon(iconName);
+        navigation.navigate(navigateTo);
+    };
+
+    return (
+
+<View>
+    <View style={styles.Title}>
+            <Text style={styles.Title}>Settings</Text>
+            <Image
+        source={require('../../assets/gear.png')} 
+        style={styles.image}
+        />
+        </View>
+
+        
+
+<View>
+    <Text></Text>
+</View>
+    <WordWithLine/>
+      <WordWithLine word="Account                                       >" onPress={() => handleIconPress('Account', 'Account')} />    
+      <WordWithLine word="Display                                         > "onPress={() => handleIconPress('Display', 'Display')} />
+      <WordWithLine word="Notifications                               >" onPress={() => handleIconPress('Notifications', 'Notifications')}/>
+      <WordWithLine word="Interaction History                    >" onPress={() => handleIconPress('Interaction', 'Interaction')}/>
+      <WordWithLine word="Contact Us                                  >" onPress={() => handleIconPress('ContactUs', 'ContactUs')}/>
+      <WordWithLineRed word="Log out                                         " />
+
+</View>
+    )}
+
+    const styles = StyleSheet.create({
+        Title: {
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+            color: 'black', fontSize: 40, fontWeight: 'bold',
+            marginLeft: 5,
+        },
+        line: {
+            height: 1,
+            backgroundColor: 'black',
+            width: '100%', // Adjust the width of the line as needed
+          },
+          image: {
+            width: 100, // Set the width of the image
+            height: 100, // Set the height of the image
+            resizeMode: 'contain', // Adjust the resizeMode property as needed
+            marginRight: 25,
+            marginTop: 10,
+          },
+          word: {
+            fontSize: 30,
+            marginBottom: 5,
+            marginLeft: 20,
+            marginTop: 10,
+          },
+          word2: {
+            fontSize: 30,
+            marginBottom: 5,
+            marginLeft: 20,
+            marginTop: 10,
+            color: 'red'
+          },
+    });
+
+    export default Sett;
