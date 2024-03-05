807fd4d7ced2b7bc869d932ec20fe68108ec8f97
Jas Chawla <jptjasc@gmail.com>
2024-03-05 09:49:30 -0500

    Updated GoToEvent

--- client/components/main/section/GoToEvent.js
+++ client/components/main/section/GoToEvent.js
@@ -8,10 +8,24 @@ const goToEvents = ({ navigation: { navigate } }) => {
 
 //Title will be what is actually displayed while dbLink woll be for connecting to the data base
 
+const dbLink = "Category Name In Route"  //category name where the event will be stored
+
+function GetTitleAndRoute()
+{
+    const response = axios.get('http://localhost:8080/api/categories')
+    .then(response => {                    //Error Catching
+      console.log("Get Request Succesful")
+      return response.data;})
+    .catch(error => {
+      console.error("Get Request Failed", error)
+    })
+
+}
+
 return(
     <View>
         <TouchableOpacity onPress={() =>
-         navigate('Events', { title: 'Event Type',  dbLink:'Da'})}>
+         navigate('Events', { title: 'Event Category Name',  dbLink:dbLink})}>
             <Text>1st event types</Text>
         </TouchableOpacity>
     </View>
