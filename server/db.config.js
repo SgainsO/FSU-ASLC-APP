64bd2bc909700ddb7b0d4f8b53e23ef9801d45d7
Jas Chawla <126195012+SgainsO@users.noreply.github.com>
2024-03-13 19:16:16 -0400

    Update db.config.js

--- server/db.config.js
+++ server/db.config.js
@@ -1,10 +1,2 @@
-module.exports = 
-{
- HOST: "askew.cp6wycis6dix.us-east-1.rds.amazonaws.com",
- USER: "postgres",
- PASSWORD: "lightsync",
- DB: "event-related",
- dialect: "postgres"
-}
-
+//THIS PAGE IS A STUB. PLEASE MESSAGE CREATERS. 
 //const db = pgp('postgres://postgres:lightsync@askew.cp6wycis6dix.us-east-1.rds.amazonaws.com:5432/askew')
