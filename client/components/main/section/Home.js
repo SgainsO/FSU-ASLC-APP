221cb528eb11c03712c3f552856873f714a55bde
Matthew Echenique <mme21a@fsu.edu>
2024-03-23 17:46:10 -0400

    replacing the real home screen with my home code

--- client/components/main/section/Home.js
+++ client/components/main/section/Home.js
@@ -1,33 +1,51 @@
-import React, { useState, } from 'react';
-import { View, Button, } from 'react-native';
-import { useNavigation } from '@react-navigation/native';
-
-import CommentSection from '../modal/Comments';
+import { View, FlatList, Text, } from 'react-native';
+import React from 'react';
+import Card from '../cards/HomeEventCard';
 
 const Home = () => {
+  const getTodayDate = () => {
+    const currentDate = new Date();
+    const format = {weekday: 'long', month: '2-digit', day: '2-digit', year: '2-digit'};
+    const finalFormat = currentDate.toLocaleDateString('en-US', format);
+    return finalFormat;
+  };
+
   const containerStyle = {
     flex: 12,
     backgroundColor: 'white',
   };
 
-  const [isModalVisible, setModalVisible] = useState(false);
-  const toggleModal = () => {
-    setModalVisible(!isModalVisible);
+  const textStyle = {
+    fontSize: 30,
+    fontWeight: '600',
+    textAlign: 'center',
+    paddingTop: 20,
+    paddingBottom: 20,
   };
 
-  const navigation = useNavigation();
-
-  const navigate = (navigateTo) => {
-    navigation.navigate(navigateTo);
-  }
+  const data = [
+    {id: 0, title: 'American Psycho', backgroundImage: require('../../../assets/american_psycho.png'), interested: '69 interested', details: 'doors open at 11:00pm\nmovie starts at 12:00am',},
+    {id: 1, title: 'Undertale Game Session', backgroundImage: require('../../../assets/sans_undertale.png'), interested: '420 interested', details: 'starts at 2pm in theater',},
+  ];
 
   return (
     <View style={containerStyle}>
-      <Button title="Placeholder Comments Button" onPress={toggleModal} />
-      <CommentSection isVisible={isModalVisible} onClose={toggleModal} />
-
-      <Button title="Placeholder Settings Button" onPress={() => navigate('Settings')} />
-      <Button title="Placeholder Signout Button" onPress={() => navigate('Login')} />
+      <Text style={textStyle}>
+        Today is...{'\n'}
+        {getTodayDate()}
+      </Text>
+      <FlatList
+        data={data}
+        renderItem={({ item }) => (
+          <View style={{ flex: 1 }}>
+            <Card {...item} />
+          </View>
+        )}
+        keyExtractor={item => item.id.toString()}
+        vertical
+        contentContainerStyle={{ paddingHorizontal: 16}}
+        />
+      <Text> Home </Text>
     </View>
   );
 };
