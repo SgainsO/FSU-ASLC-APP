1de290bb2c92aabbd9680859261be87c2a6e3f53
zdeag <80681304+zdeag@users.noreply.github.com>
2024-04-04 13:30:39 -0400

    Supported Data Binding for AdminUser.js

--- client/components/main/section/AdminUsers.js
+++ client/components/main/section/AdminUsers.js
@@ -1,5 +1,6 @@
-import React, { useState, } from 'react';
+import React, { useEffect, useState, } from 'react';
 import { View, StyleSheet, TouchableOpacity } from 'react-native';
+import axios from 'axios';
 
 import AdminTable from '../tables/AdminTable';
 import SearchBar from '../SearchBar';
@@ -11,39 +12,32 @@ const AdminUsers = () => {
   // Search bar consts
   const [searchPhrase, setSearchPhrase] = useState("");
   const [clicked, setClicked] = useState(false);
-    
-  state = {
+  const [userData, setUserData] = useState({
     tableHead: ['Image', 'ID', 'Name', 'Email', 'Actions'],
-    tableData: [
-      ['https://c.stocksy.com/a/bBo600/z9/1622887.jpg', '0', 'Daniel Dang', 'dtd21b@fsu.edu'],
-      ['https://i.pinimg.com/originals/80/fb/8d/80fb8d8390601bfe5ba4c52b4dc79b8a.jpg', '1', 'Zachary De Aguiar', 'zzd21@fsu.edu'],
-      ['https://i.pinimg.com/236x/47/51/48/475148587abdbdd81cc3d09fdbcbab16.jpg', '2', 'Jas Chawla', 'jsc21b@fsu.edu'],
-      ['https://th.bing.com/th/id/R.57529fe941352b6a608aa0c77cacc099?rik=ja%2f2y3qTNUZQVQ&riu=http%3a%2f%2fwww.pak101.com%2ffunnypictures%2fFunny%2f2011%2f7%2f22%2f5_cjlxs.jpg&ehk=8WobPJQV0XRZ0iGg7UzDSOJbCsygoHpsoYYUz7WoF9o%3d&risl=&pid=ImgRaw&r=0.jpg', '3', 'Ryan Nageer', 'rfn22@fsu.edu'],
-      ['https://th.bing.com/th/id/OIP.3GZTipqj8i7LniAr6IgDdwAAAA?rs=1&pid=ImgDetMain.jpg', '4', 'Matthew Echenique', 'mme21a@fsu.edu'],
-      ['https://upload.wikimedia.org/wikipedia/commons/thumb/a/ad/Placeholder_no_text.svg/600px-Placeholder_no_text.svg.png', '5', 'Alex Morgan', 'temp@gmail.com'],
-      ['https://upload.wikimedia.org/wikipedia/commons/thumb/a/ad/Placeholder_no_text.svg/600px-Placeholder_no_text.svg.png', '6', 'Jordan Casey', 'temp@gmail.com'],
-      ['https://upload.wikimedia.org/wikipedia/commons/thumb/a/ad/Placeholder_no_text.svg/600px-Placeholder_no_text.svg.png', '8', 'Jamie Park', 'temp@gmail.com'],
-      ['https://upload.wikimedia.org/wikipedia/commons/thumb/a/ad/Placeholder_no_text.svg/600px-Placeholder_no_text.svg.png', '7', 'Taylor Reed', 'temp@gmail.com'],
-      ['https://upload.wikimedia.org/wikipedia/commons/thumb/a/ad/Placeholder_no_text.svg/600px-Placeholder_no_text.svg.png', '9', 'Casey Lee', 'temp@gmail.com'],
-      ['https://upload.wikimedia.org/wikipedia/commons/thumb/a/ad/Placeholder_no_text.svg/600px-Placeholder_no_text.svg.png', '10', 'Riley Quinn', 'temp@gmail.com'],
-      ['https://upload.wikimedia.org/wikipedia/commons/thumb/a/ad/Placeholder_no_text.svg/600px-Placeholder_no_text.svg.png', '11', 'Dakota Ellis', 'temp@gmail.com'],
-      ['https://upload.wikimedia.org/wikipedia/commons/thumb/a/ad/Placeholder_no_text.svg/600px-Placeholder_no_text.svg.png', '12', 'Parker Grey', 'temp@gmail.com'],
-      ['https://upload.wikimedia.org/wikipedia/commons/thumb/a/ad/Placeholder_no_text.svg/600px-Placeholder_no_text.svg.png', '13', 'Morgan Bailey', 'temp@gmail.com'],
-      ['https://upload.wikimedia.org/wikipedia/commons/thumb/a/ad/Placeholder_no_text.svg/600px-Placeholder_no_text.svg.png', '14', 'Quinn Avery', 'temp@gmail.com'],
-      ['https://upload.wikimedia.org/wikipedia/commons/thumb/a/ad/Placeholder_no_text.svg/600px-Placeholder_no_text.svg.png', '15', 'Charlie Jordan', 'temp@gmail.com'],
-      ['https://upload.wikimedia.org/wikipedia/commons/thumb/a/ad/Placeholder_no_text.svg/600px-Placeholder_no_text.svg.png', '16', 'Skyler Pat', 'temp@gmail.com'],
-      ['https://upload.wikimedia.org/wikipedia/commons/thumb/a/ad/Placeholder_no_text.svg/600px-Placeholder_no_text.svg.png', '17', 'Robin Blake', 'temp@gmail.com'],
-      ['https://upload.wikimedia.org/wikipedia/commons/thumb/a/ad/Placeholder_no_text.svg/600px-Placeholder_no_text.svg.png', '18', 'Jesse Cameron', 'temp@gmail.com'],
-      ['https://upload.wikimedia.org/wikipedia/commons/thumb/a/ad/Placeholder_no_text.svg/600px-Placeholder_no_text.svg.png', '19', 'Avery Sam', 'temp@gmail.com'],
-      ['https://upload.wikimedia.org/wikipedia/commons/thumb/a/ad/Placeholder_no_text.svg/600px-Placeholder_no_text.svg.png', '20', 'Rowan Alex', 'temp@gmail.com'],
-      ['https://upload.wikimedia.org/wikipedia/commons/thumb/a/ad/Placeholder_no_text.svg/600px-Placeholder_no_text.svg.png', '21', 'Drew Jordan', 'temp@gmail.com'],
-      ['https://upload.wikimedia.org/wikipedia/commons/thumb/a/ad/Placeholder_no_text.svg/600px-Placeholder_no_text.svg.png', '22', 'Sawyer Lee', 'temp@gmail.com'],
-      ['https://upload.wikimedia.org/wikipedia/commons/thumb/a/ad/Placeholder_no_text.svg/600px-Placeholder_no_text.svg.png', '23', 'Peyton Chris', 'temp@gmail.com'],
-      ['https://upload.wikimedia.org/wikipedia/commons/thumb/a/ad/Placeholder_no_text.svg/600px-Placeholder_no_text.svg.png', '24', 'Kai Morgan', 'temp@gmail.com'],
-    ],
+    tableData: [],
     widthPercents: [15, 15, 22, 28, 20],
     type: 'User'
-  }
+  });
+  
+  useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+        const response = await axios.get('http://localhost:3000/api/getUsers');
+        const formattedData = response.data.data.map(user => [
+          user.url,
+          user.id.toString(),
+          `${user.firstname} ${user.lastname}`,
+          user.email,
+        ]);
+        console.log('formattedData: ', formattedData);
+        setUserData(prevState => ({ ...prevState, tableData: formattedData }));
+      } catch (error) {
+        console.error('Error retrieving users: ', error);
+      }
+    };
+    fetchUsers();
+  }, []);
+
 
   const [isModalVisible, setModalVisible] = useState(false);
   const toggleModal = () => {
@@ -52,7 +46,7 @@ const AdminUsers = () => {
 
   return (
     <View style={styles.container}>
-      <AdminCreation isModalVisible={isModalVisible} setModalVisible={setModalVisible} type={state.type} />
+      <AdminCreation isModalVisible={isModalVisible} setModalVisible={setModalVisible} type="User" />
       <View style={styles.topContainer}>
         <SearchBar
           searchPhrase={searchPhrase}
@@ -60,11 +54,11 @@ const AdminUsers = () => {
           clicked={clicked}
           setClicked={setClicked}
         />
-        <TouchableOpacity onPress={() => toggleModal()}>
+        <TouchableOpacity onPress={toggleModal}>
           <Entypo name="squared-plus" size={40} color="#27ae60" />
         </TouchableOpacity>
       </View>
-      <AdminTable state={state}/>
+      <AdminTable state={userData}/>
     </View>
   );
 };
