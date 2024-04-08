62ff2a0eab1555884917f46e7228e75df5c3e2d7
zdeag <deaguiarzachary@gmail.com>
2024-04-08 17:38:28 -0400

    Added the ability to change userToken and listen to changes

--- client/components/AuthProvider.js
+++ client/components/AuthProvider.js
@@ -1,4 +1,6 @@
-import React, { createContext, useContext, useState } from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import React, { createContext, useContext, useEffect, useState } from 'react';
+import { useNavigation} from '@react-navigation/native';
 
 // Create context
 const AuthContext = createContext();
@@ -6,9 +8,42 @@ const AuthContext = createContext();
 // Provider component
 export const AuthProvider = ({ children }) => {
   const [isAdmin, setIsAdmin] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+  const [userToken, setUserToken] = useState(null);
+
+  const handleLogin = () => {
+    setUserToken('testoken');
+    AsyncStorage.setItem('userToken', 'testoken');
+    console.log('Set user token to AsyncStorage');
+  }
+
+  // TODO: Implement register
+  const handleRegister = () => {
+    setUserToken('testoken');
+    AsyncStorage.setItem('userToken', 'testoken');
+  }
+
+  const handleLogout = () => {
+    setUserToken(null);
+    AsyncStorage.removeItem('userToken');
+
+    console.log("User has been logged out.")
+  }
+
+  const isLoggedIn = async () => {
+    const token = await AsyncStorage.getItem('userToken');
+    if (token) {
+      setUserToken(token);
+      console.log('User is logged in')
+    }
+  }
+
+  useEffect(() => {
+    isLoggedIn();
+  }, []);
 
   return (
-    <AuthContext.Provider value={{ isAdmin, setIsAdmin }}>
+    <AuthContext.Provider value={{ isAdmin, setIsAdmin, userToken, handleLogin, isLoggedIn, handleLogout}}>
       {children}
     </AuthContext.Provider>
   );
--- client/components/main/section/Home.js
+++ client/components/main/section/Home.js
@@ -1,11 +1,20 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity} from 'react-native';
 import { useColorSchemeContext } from '../ColorSchemeContext';
+
+import { useAuth } from '../../AuthProvider';
+
 const Home = () => {
   const { colorScheme, toggleColorScheme } = useColorSchemeContext();
+
+  const { handleLogout } = useAuth();
+
   return (
     <View style={[styles.container, colorScheme === 'dark' && styles.darkContainer]}>
       <Text style={[styles.text, colorScheme === 'dark' && styles.darkText]}>This is a template screen</Text>
+        <TouchableOpacity style={styles.button} onPress={handleLogout}>
+          <Text style={styles.buttonText}>Logout</Text>
+        </TouchableOpacity>
     </View>
   );
 };
