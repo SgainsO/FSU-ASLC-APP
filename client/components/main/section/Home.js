8a980bdcc5fb1b8cfc8ab00493d3c2e997cb95e0
Jas Chawla <jptjasc@gmail.com>
2024-04-27 12:04:01 -0400

    Fixed Scroll End

--- client/components/main/section/Home.js
+++ client/components/main/section/Home.js
@@ -199,6 +199,7 @@ const Home = ({ route }) => {
       console.log("key" + activeButton);
       SetLiked(fetchData());
       fetchEvents();
+      setEndReached(false);
 
     }, [activeButton])
 
